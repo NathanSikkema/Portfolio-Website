12738641c7bdda116507c6d5562c8d85132539ae
Nathan Sikkema <nathan.sikkema@mohawkcollege.ca>
2025-02-28 15:13:08 -0500

    Adding all starter files to the repository

---
+++ README.md
@@ -0,0 +1,10 @@
+# Assignment 6: Portfolio Website
+
+This website includes the following information about me:
+ - Summarizing Statement
+ - Education
+ - Skills
+ - Projects
+ - If you do not have any projects you feel you can list, you can make up some "pretend" projects!
+ - Resume
+ - Contact Information
---
+++ js/functions.js
@@ -0,0 +1,10 @@
+// Author............ : Nathan Sikkema
+// Student Number.... : 000911571
+// Program........... : Software Development
+// Course............ : Software Development Tools
+// Type.............. : JS
+// Start Date........ : 
+// End Date.......... : 
+// Duration.......... : 
+// Last Modified..... : 
+// Validation Date... : 
